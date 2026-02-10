4d53d2dead8a07c92cd2f804e8d1b9d4e1676a8e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-10 21:11:26 +0100

    buffers in action

---
+++ buffers/challenge.js
@@ -0,0 +1,12 @@
+// 0100 1000 0110 1001 0010 0001
+
+const { Buffer } = require('buffer');
+
+const buff = Buffer.alloc(3)
+
+buff[0] = 0x48;
+buff[1] = 0x69;
+buff[2] = 0x21;
+
+console.log(buff)
+console.log(buff.toString('utf-8'))

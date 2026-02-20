49e81987a17ebb738f7b0c1a828bc4bc25513df5
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-02-20 13:53:33 +0100

    feat(file-system): read buffer data

--- file-system/app.js
+++ file-system/app.js
@@ -3,29 +3,27 @@ const fs = require('fs/promises');
 (async () => {
   const commandFileHandler = await fs.open('./command.txt', 'r');
 
-  const watcher = fs.watch('./command.txt');
+  commandFileHandler.on('change', async () => {
+    // Get the size of our file and allocate buffer
+    const size = (await commandFileHandler.stat()).size;
+    const buff = Buffer.alloc(size);
 
+    const offset = 0; // Location at which to start filling our buffer
+    const length = buff.byteLength; // Bytes to read
+
+    const position = 0; // Position to start reading the file from
+
+    // We want to read the content
+    await commandFileHandler.read(buff, offset, length, position);
+
+    console.log(buff.toString('utf-8'));
+  });
+
+  // Watcher
+  const watcher = fs.watch('./command.txt');
   for await (const event of watcher) {
     if (event.eventType === 'change') {
-      // The file was changed
-      console.log('The file was changed');
-
-      // Get the size of our file
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-
-      const offset = 0;
-      const length = buff.byteLength;
-      const position = 0;
-
-      // We want to read the content
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position,
-      );
-      console.log("Content",content);
+      commandFileHandler.emit('change');
     }
   }
 })();

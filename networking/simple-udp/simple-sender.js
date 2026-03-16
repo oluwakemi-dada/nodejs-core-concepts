95f7636f198845201cf9ab010f7fa3fce5b05e57
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-16 19:28:50 +0100

    feat(net): simple udp

---
+++ networking/simple-udp/simple-sender.js
@@ -0,0 +1,13 @@
+const dgram = require('dgram');
+
+const sender = dgram.createSocket('udp4');
+
+sender.send('This is a string', 8000, '127.0.0.1', (error, bytes) => {
+  if (error) console.log(error);
+  console.log(bytes);
+});
+
+sender.send('This is another string', 8000, '127.0.0.1', (error, bytes) => {
+  if (error) console.log(error);
+  console.log(bytes);
+});

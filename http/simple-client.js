01d6d58d7166189bfa40974aad1b3625ec769578
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-23 13:51:27 +0100

    feat(http): sending a response to the client

--- http/simple-client.js
+++ http/simple-client.js
@@ -1,27 +1,40 @@
-const http = require("node:http");
+const http = require('node:http');
 
 const agent = new http.Agent({ keepAlive: true });
 
 const request = http.request({
   agent: agent,
-  hostname: "localhost",
+  hostname: 'localhost',
   port: 8050,
-  method: "POST",
-  path: "/create-post",
+  method: 'POST',
+  path: '/create-post',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
+    name: 'Joe',
   },
 });
 
 // This event is emitted only once
-request.on("response", (response) => {});
+request.on('response', (response) => {
+  console.log('--------- STATUS: ---------');
+  console.log(response.statusCode);
 
-request.write(JSON.stringify({ message: "Hi there!" }));
-request.write(JSON.stringify({ message: "How are you doing?" }));
-request.write(JSON.stringify({ message: "Hey you still there?" }));
+  console.log('--------- HEADERS: ---------');
+  console.log(response.headers);
+
+  console.log('--------- BODY: ---------');
+  response.on('data', (chunk) => {
+    console.log(chunk.toString('utf-8'));
+  });
+
+  response.on('end', () => {
+    console.log('No more data in response');
+  });
+});
 
 request.end(
   JSON.stringify({
-    message: "This is going to be my last message!",
-  })
+    title: 'Title of my post',
+    body: 'This is some text and more and more',
+  }),
 );
--- http/simple-server.js
+++ http/simple-server.js
@@ -1,24 +1,41 @@
-const http = require("node:http");
+const http = require('node:http');
 
 const server = http.createServer();
 
-server.on("request", (request, response) => {
-  console.log("--------- METHOD: ---------");
+server.on('request', (request, response) => {
+  console.log('--------- METHOD: ---------');
   console.log(request.method);
 
-  console.log("--------- URL: ---------");
+  console.log('--------- URL: ---------');
   console.log(request.url);
 
-  console.log("--------- HEADERS: ---------");
+  console.log('--------- HEADERS: ---------');
   console.log(request.headers);
+  const name = request.headers.name;
 
-  console.log("--------- BODY: ---------");
+  console.log('--------- BODY: ---------');
 
-  request.on("data", (chunk) => {
-    console.log(chunk.toString("utf-8"));
+  let data = '';
+
+  request.on('data', (chunk) => {
+    data += chunk.toString();
+  });
+
+  request.on('end', () => {
+    data = JSON.parse(data);
+
+    console.log(data);
+    console.log(name);
+
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    response.end(
+      JSON.stringify({ message: `Post with title ${data.title} was created by ${name}` }),
+    );
   });
 });
 
 server.listen(8050, () => {
-  console.log("Server listening on http://localhost:8050");
+  console.log('Server listening on http://localhost:8050');
 });

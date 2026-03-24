761445975a0b835dd65e00bf1a810eac1ba8dbab
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-24 18:43:54 +0100

    add some json routes

--- http/simple-web/server.js
+++ http/simple-web/server.js
@@ -22,14 +22,38 @@ server.on('request', async (request, response) => {
     fileStream.pipe(response);
   }
 
-    if (request.url === '/script.js' && request.method === 'GET') {
-      response.setHeader('Content-Type', 'text/javascript');
+  if (request.url === '/script.js' && request.method === 'GET') {
+    response.setHeader('Content-Type', 'text/javascript');
 
-      const fileHandle = await fs.open('./public/script.js', 'r');
-      const fileStream = fileHandle.createReadStream();
+    const fileHandle = await fs.open('./public/script.js', 'r');
+    const fileStream = fileHandle.createReadStream();
 
-      fileStream.pipe(response);
-    }
+    fileStream.pipe(response);
+  }
+
+  if (request.url === '/login' && request.method === 'POST') {
+    response.setHeader('Content-Type', 'application/json');
+    response.statusCode = 200;
+
+    const body = {
+      message: 'Logging you in...',
+    };
+
+    response.end(JSON.stringify(body));
+  }
+
+  if (request.url === '/user' && request.method === 'PUT') {
+    response.setHeader('Content-Type', 'application/json');
+    response.statusCode = 401;
+
+    const body = {
+      message: 'You have to login first.',
+    };
+
+    response.end(JSON.stringify(body));
+  }
+
+  // upload route
 });
 
 server.listen(9000, () => {

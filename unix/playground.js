dde59cc5be0bede4989e7f2bf235055ef4a42dab
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-09 18:54:31 +0100

    unix: stdout, stdin, stderr

--- unix/playground.js
+++ unix/playground.js
@@ -1,15 +1,27 @@
 const { spawn, exec } = require('node:child_process');
+const { stdin, stdout, stderr } = require('node:process');
 
-// console.log(process.env.PATH);
-console.log(process.argv);
-console.log(process.pid);
-
-const subprocess = spawn('/Applications/Postman.app/Contents/MacOS/Postman');
-// const subprocess = spawn('open', ['-a', 'Notes']);
-
-subprocess.stdout.on('data', (data) => {
-  console.log(data.toString('utf-8'));
+stdin.on('data', (data) => {
+  // console.log('Got this data from standard in: ', data.toString('utf-8'));
+  stdout.write(`Got this data from standard in: ${data.toString('utf-8')}\n`);
 });
+
+stdout.write('This is some text that i want.');
+
+stderr.write('This is some text that i may not want.');
+
+// ---- Process variables
+// console.log(process.env.PATH);
+// console.log(process.argv);
+// console.log(process.pid);
+
+// ---- Spawning a process
+// const subprocess = spawn('/Applications/Postman.app/Contents/MacOS/Postman');
+// // const subprocess = spawn('open', ['-a', 'Notes']);
+
+// subprocess.stdout.on('data', (data) => {
+//   console.log(data.toString('utf-8'));
+// });
 
 // exec("echo 'something string' | tr ' ' '\n'", (error, stdout, stderr) => {
 //   if (error) {

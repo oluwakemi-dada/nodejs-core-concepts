f47ff248bedecb7eee3cb7b032b2354738585792
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-04-10 13:12:42 +0100

    unix: communication app c part and node alternative

---
+++ unix/learning-c/app.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int a = 20;
+  int b = 30;
+  int c = 20 + 30;
+
+  fprintf(stdout, "This is some string");
+
+  return 0;
+}
--- unix/massive-communication/app.js
+++ unix/massive-communication/app.js
@@ -1,7 +1,7 @@
 const { spawn } = require('node:child_process');
 const fs = require('node:fs');
 
-const numberFormatter = spawn('number_formatter', ['./dest.txt', '$', ',']);
+const numberFormatter = spawn('./number_formatter', ['./dest.txt', '$', ',']);
 
 numberFormatter.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
@@ -19,7 +19,11 @@ numberFormatter.on('close', (code) => {
   }
 });
 
-(async () => {
-  const fileStream = fs.createReadStream('./src.txt');
-  fileStream.pipe(numberFormatter.stdin);
-})();
+// Users/oluwakemi/Desktop/text-gigantic.txt
+const fileStream = fs.createReadStream('./src.txt');
+fileStream.pipe(numberFormatter.stdin);
+
+// numberFormatter.stdin.write("324 8236 4238");
+// numberFormatter.stdin.write("3123 24 8236 4238");
+// numberFormatter.stdin.write("324 12 38236 4238");
+// numberFormatter.stdin.end("321 234 8236 4231 23128"); // sending EOF sign
